70bfa66c645f02c5d9cf6ef2a24c9329dd67dd7c
杨超 <1659514328@qq.com>
2014-12-20 10:01:19 +0800

    commit

--- main.c
+++ main.c
@@ -12,12 +12,14 @@
 /*********************************************************************************************/
 
 
-
 #include <STC12C2052AD.H>
 
 sbit LED = P1 ^ 1;
 sbit BTN = P1 ^ 2;
 
+//100微秒级别延时程序
+//由于使用的是12mhz晶体，因此每60次脉冲为0.00001秒，
+//也就是0.1毫秒，100微秒	    
 void delay(unsigned int a){
 	unsigned int i;
 	while(--a != 0 ){
@@ -36,7 +38,7 @@ void main(void){
 	int brightness = 0;
 	int step = 2;
 	while(1){
-		millis ++;
+		millis ++; //增加一个计时器，每次脉冲都加一
 		reading = BTN;
 		if(reading != lastBtnState){
 			lastDebounceTime = millis;
@@ -56,7 +58,7 @@ void main(void){
 				}
 			}
 		}
-		//delay(1);
+		//通过pwm脉宽调制，将灯泡的亮度进行调整
 		if(brightness < 10){
 			LED = 1;
 			delay(10 - brightness);
